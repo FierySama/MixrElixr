2ffe458544b222ab6ce6b6bf17e0bae1e7115c02
FierySama <fieryludus@gmail.com>
2019-07-10 21:01:21 -0700

    Kill me now
    i dont understand this, just saving so i dont lose any progress

--- injected-content/mixrelixr.js
+++ injected-content/mixrelixr.js
@@ -117,8 +117,36 @@ $(() => {
 		}
 	}
 
+// function to be called to activate search bar instead of moving the search bar up, currently should activate only the top bar to be non transparent
+/*function toggleHomePageTopBar() {
+	var element = document.getElementById("b-desktop-header");
+	element.classList.toggle("collapsed");
+  }
+*/
+
 	function loadHomepage(){
 		log('Loading up settings for homepage');
+
+		///////Test code to always move search bar to top, hard coded. Unless we want the user to pick, which i doubt anyone wants it half way through the page
+		{
+			log('Search Bar code loaded');
+			$("b-desktop-header").addClass("collapsed");
+			//let searchBar = $(".layout-align-start-center");
+			let searchBar = $("b-browse-channel-header").children().first();
+			let b = 1;
+			if (b === 1) {
+				searchBar.offset(); = '-5000px';
+				log('attempted bar styling')
+			} else {
+				log ('Nothing happened')
+			}
+			
+		} 
+		/*window.onload = function(){
+			log('Search Bar Event Listener worked');
+			document.getElementById("").style.top  = '-2000px';
+			document.getElementById("foo").getElementsByClassName("bar")[0
+		};*/
 
 		// If the user desires to have favorites highlighted:
 		if(settings.generalOptions.highlightFavorites){
